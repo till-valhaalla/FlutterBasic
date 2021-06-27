5510b0b14b54d48aaafecf8c85d00481fe10640e
Fatima <fatimakhalidipod@live.com>
2021-06-27 04:29:56 +0100

    DAY-2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_11/homepage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Welcome to hell'),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
